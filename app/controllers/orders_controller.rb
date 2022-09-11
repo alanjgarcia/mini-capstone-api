22d8858be48f369318299eeb7c7227a96e117cf1
Alan Garcia <alangarciacoding@gmail.com>
2022-09-11 12:24:42 -0500

    Updated Orders controller create method

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,30 @@
+class OrdersController < ApplicationController
+  def create
+    product = Product.find_by(id: params[:product_id])
+    price = product.price
+    calculated_subtotal = price * params[:quantity].to_i
+    calculated_tax = product.tax * params[:quantity].to_i
+    calculated_total = calculated_subtotal + calculated_tax
+
+    order = Order.new(
+      user_id: current_user.id,
+      product_id: params[:product_id],
+      quantity: params[:quantity],
+      subtotal: calculated_subtotal,
+      tax: calculated_tax,
+      total: calculated_total,
+    )
+    order.save
+    render json: order.as_json
+  end
+
+  def show
+    order = Order.find_by(id: params[:id])
+    render json: order.as_json
+  end
+
+  def index
+    orders = Order.all
+    render json: orders.as_json
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  post "/orders" => "orders#create"
+  get "/orders/:id" => "orders#show"
+  get "/orders" => "orders#index"
+
   post "/users" => "users#create"
   post "/sessions" => "sessions#create"
+
   get "/products" => "products#index"
   post "/products" => "products#create"
   get "/products/:id" => "products#show"
